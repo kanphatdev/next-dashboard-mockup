230c389946895293cacfdb1526c9c06117245658
kanphatratrazer <kanphatratrazer28@gmail.com>
2023-08-08 07:56:00 +0700

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,17 @@
+"use client";
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between px-4 pt-4'>
+      <h2 className='capitalize'>
+        dashboard
+      </h2>
+      <h2 className='capitalize'>
+        welcome back
+      </h2>
+    </div>
+  )
+}
+
+export default Header
